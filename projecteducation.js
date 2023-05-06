035684061ed6daff80bef16107b0e854ef732634
Abdul-Ahad <121967743+ahad324@users.noreply.github.com>
2023-05-07 01:18:29 +0500

    Add files via upload

--- projecteducation.js
+++ projecteducation.js
@@ -10,3 +10,13 @@ AOS.init({
     duration: 1000,
     easing: 'ease',
 });
+
+const cursor = document.querySelector("#cursor");
+
+document.addEventListener("mousemove", (e) => {
+	let x = e.clientX;
+	let y = e.clientY;
+
+	cursor.style.left = x + "px";
+	cursor.style.top = y + "px";
+});
